6553280a7cc18b490ab11328a77bc608b46a12bf
Bennett Desk <bdeskovich17a@farragut2.org>
2026-04-28 11:29:07 -0400

    update app

---
+++ public/version.js
@@ -0,0 +1,6 @@
+/**
+ * App deployment version marker.
+ * Update APP_VERSION for each deploy to verify cache + rollout.
+ */
+window.APP_VERSION = '1.0.0';
+window.APP_BUILD_TIMESTAMP = '2026-04-28 11:27';
